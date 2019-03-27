650e74421e81afe4b6b6406a858f1b530f7a6dc3
fanbotao <fan.fan@shebaochina.com>
2019-03-27 20:43:44 +0800

    mybatis-plus-genertor

--- manage-service/src/main/java/com/fw/manage/web/controller/TestController.java
+++ manage-service/src/main/java/com/fw/manage/web/controller/TestController.java
@@ -1,9 +1,16 @@
 package com.fw.manage.web.controller;
 
 
+import com.fw.manage.entity.Test;
+import com.fw.manage.service.TestService;
+import com.fw.manage.web.domain.HttpResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -13,9 +20,20 @@ import org.springframework.stereotype.Controller;
  * @author MP-ML
  * @since 2019-03-27
  */
-@Controller
+@RestController
 @RequestMapping("/test")
 public class TestController {
 
+    @Resource
+    private TestService testService;
+
+    @PostMapping("/insertSelective")
+    public HttpResult insertSelective(Test test) {
+        if (test == null) {
+            HttpResult.failure(400, "缺少参数");
+        }
+        HttpResult httpResult = testService.insertSelective(test);
+        return httpResult;
+    }
 }
 
