23117ac0777d433814ec9a21929522ea8f70947b
fanbotao <fan.fan@shebaochina.com>
2019-03-28 17:47:42 +0800

    Springboot-Scheduling

--- manage-service/src/main/java/com/fw/manage/ManageServiceApplication.java
+++ manage-service/src/main/java/com/fw/manage/ManageServiceApplication.java
@@ -3,9 +3,11 @@ package com.fw.manage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.fw.manage.dao")
+@EnableScheduling
 public class ManageServiceApplication {
 
     public static void main(String[] args) {
---
+++ manage-service/src/main/java/com/fw/manage/task/TimedTask.java
@@ -0,0 +1,25 @@
+/*
+package com.fw.manage.task;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component
+public class TimedTask {
+
+    @Value("hello")
+    private String hello;
+
+    @Value("world")
+    private String world;
+
+    @Scheduled(cron = "0 /1 * * * ?")
+    public void execute() {
+        System.out.println(hello + " : Pock Face");
+        System.out.println(world + " : charming T");
+    }
+}
+*/
