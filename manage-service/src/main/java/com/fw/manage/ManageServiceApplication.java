38618125099362107a9a07aa78447c1aba1491ca
fanbotao <fan.fan@shebaochina.com>
2019-04-01 17:18:54 +0800

    dynamic-datasource

--- manage-service/src/main/java/com/fw/manage/ManageServiceApplication.java
+++ manage-service/src/main/java/com/fw/manage/ManageServiceApplication.java
@@ -1,11 +1,12 @@
 package com.fw.manage;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @MapperScan("com.fw.manage.dao")
 @EnableScheduling
 public class ManageServiceApplication {
